6139b2cbe09f112d800fd00c283b0d811455549a
Gor <7495500@gmail.com>
2023-04-23 18:48:13 +0300

    Add basic app configure

---
+++ db/migrate/20230423133511_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.string :url
+      t.boolean :purchased
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+categories = %w[
+  Groceries
+  Electronics
+  Clothing
+]
+
+categories.each do |category|
+  cat = Category.create(name: category)
+
+  cat.products.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price,
+    url: Faker::Internet.url,
+    purchased: [ true, false ].sample
+  )
+end
